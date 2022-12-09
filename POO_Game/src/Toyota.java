3324dcf961608fc9c03dfa2e60e72885d7fbc405
Ynafets <stefanyj1609@gmail.com>
2022-12-08 21:56:50 -0500

    Añadiendo PooGame a dev2

---
+++ POO_Game/src/Toyota.java
@@ -0,0 +1,9 @@
+public class Toyota extends Auto{
+
+    Toyota(String sPlaca){
+        //Inicializando
+        super(sPlaca);
+        super.setdVelocidadMaxima(200);
+        super.setsPlaca("P1v102");
+    }
+}
